fcb754634462c80b8aa310105541ad363f3633b9
bilalhayni <bilal.hyni@gmail.com>
2026-01-03 19:38:16 +0100

    Fix Er

--- src/main/java/com/example/labomasi/model/dto/form/MemberEditForm.java
+++ src/main/java/com/example/labomasi/model/dto/form/MemberEditForm.java
@@ -1,6 +1,9 @@
 package com.example.labomasi.model.dto.form;
 
-import jakarta.validation.constraints.*;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
